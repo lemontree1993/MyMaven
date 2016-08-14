1202c662e42003cbc2c8d99365afe3faef80b612
lemontree1993 <1330156482@qq.com>
2016-08-14 23:21:04 +0800

    添加前端模板到项目中来

--- src/main/java/com/lemontree/test/Test.java
+++ src/main/java/com/lemontree/test/Test.java
@@ -1,7 +1,17 @@
 package com.lemontree.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by lemontree on 2016/8/8.
  */
 public class Test {
+    private String test;
+    public static void main(String[] args) {
+        System.out.println("1");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-mybatis.xml");
+        MySpring t = (MySpring) context.getBean("myspring");
+        System.out.println(t.getName());
+    }
 }
