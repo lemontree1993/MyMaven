300a95abc65a9b9b34e49885113c59e555d89f9e
lemontree1993 <1330156482@qq.com>
2016-08-17 23:41:22 +0800

    初步搭建完成项目！！！！！

--- src/main/java/com/lemontree/test/Test.java
+++ src/main/java/com/lemontree/test/Test.java
@@ -9,9 +9,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     private String test;
     public static void main(String[] args) {
-        System.out.println("1");
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-mybatis.xml");
-        MySpring t = (MySpring) context.getBean("myspring");
-        System.out.println(t.getName());
+
     }
 }
