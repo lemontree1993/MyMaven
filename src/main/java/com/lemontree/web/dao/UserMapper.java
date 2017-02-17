59fdc0d444926b26105fbb5c6dbdd3a3d3326eb3
yangchuanyun <yangchuanyun@zhuojianchina.com>
2017-02-17 16:37:43 +0800

    添加登录模态框

--- src/main/java/com/lemontree/web/dao/UserMapper.java
+++ src/main/java/com/lemontree/web/dao/UserMapper.java
@@ -1,9 +1,7 @@
 package com.lemontree.web.dao;
 
+import com.lemontree.core.base.BaseDao;
 import com.lemontree.web.entity.User;
 
-public interface UserMapper {
-    int insert(User record);
-
-    int insertSelective(User record);
+public interface UserMapper extends BaseDao<User>{
 }
